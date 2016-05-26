e281abd72f66914e3b97b22afc8e9668b020c75b
Стас Ворожка < >
2016-05-26 18:16:22 +0300

    add Executors

--- src/Demo.java
+++ src/Demo.java
@@ -10,6 +10,7 @@ public class Demo {
     public static void main(String[] args) {
 
         FileWatcher fileWatcher = new FileWatcher(new File("Files"));
+        fileWatcher.getFiles();
         FutureTask<Integer> future = new FutureTask<>(fileWatcher);
         Thread thread = new Thread(future);
         thread.start();
--- src/MultithreadWordCounter/FileWatcher.java
+++ src/MultithreadWordCounter/FileWatcher.java
@@ -5,9 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 /**
  * Created by Стас on 26.05.2016.
@@ -17,22 +15,40 @@ public class FileWatcher implements Callable<Integer> {
     private File directory;
     private List<Future<Integer>> futures = new ArrayList<>();
     private Integer wordCounter = 0;
+    private static ExecutorService exec = Executors.newFixedThreadPool(2);
+    /**
+     * обробатываем по два потока
+     * т.к. два ядра у меня)
+     * да и проще отследить роботу
+     */
 
     public FileWatcher(File directory) {
         this.directory = directory;
     }
 
+    //вынес в отдельный метод что-бы не заходило в файлы..
+    public File[] getFiles(){
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            FutureTask<Integer> future = new FutureTask<>(new FileWatcher(file));
+            futures.add(future);
+            exec.execute(future);
+        }
+        exec.shutdown();
+        return files;
+    }
+
     @Override
     public Integer call() throws Exception {
-        if (directory.isDirectory()){
-            File[] files = directory.listFiles();
-            for (File file : files) {
-                FutureTask<Integer> future = new FutureTask<>(new FileWatcher(file));
-                futures.add(future);
-                new Thread(future).start();
-            }
-        }
-        else {
+
+        System.out.println(Thread.currentThread().getName());
+        /**
+         *Пытался понять какие потоки входят, и сколько их
+         *и окозалось что пропускает он по два, все в порядке
+         */
+
+        //в файл теперь заходить не будем, посчитаем все корректно
+        if(directory.isFile()) {
             String textLine;
             try(BufferedReader reader = new BufferedReader(new FileReader(directory))){
                 while((textLine = reader.readLine())!=null){
@@ -41,8 +57,13 @@ public class FileWatcher implements Callable<Integer> {
             }
             return wordCounter;
         }
+
         for (Future<Integer> future : futures) {
             wordCounter += future.get();
+            /**
+             *get блокирует выполнения програмы до тех пор пока не
+             *получим отклик от Future
+             */
         }
         return wordCounter;
     }
