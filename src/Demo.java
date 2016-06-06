3a2573f7bf47697807ee5811b9b90ea2797a56b6
Стас Ворожка < >
2016-06-06 20:00:37 +0300

    Counter can read files

--- src/Demo.java
+++ src/Demo.java
@@ -1,4 +1,4 @@
-import MultithreadWordCounter.FileWatcher;
+import MultithreadWordCounter.WordCounter;
 import java.io.*;
 
 /**
@@ -6,8 +6,13 @@ import java.io.*;
  */
 public class Demo {
     public static void main(String[] args) {
-        try (FileWatcher fileWatcher = new FileWatcher(new File("Files"))) {
-            System.out.println("Count of word - " + fileWatcher.startThreads());
+        /**
+         * Если указать не существующий файл,
+         * вылетит с метода startThreads(),
+         * и здесь выведет null
+         */
+        try (WordCounter fileWatcher = new WordCounter(new File("Files"))) {
+            System.out.println("Count of word - " + fileWatcher.startThreadsWithDirectory());
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
--- src/MultithreadWordCounter/WordCounter.java
+++ src/MultithreadWordCounter/WordCounter.java
@@ -1,5 +1,7 @@
 package MultithreadWordCounter;
 
+import com.sun.istack.internal.Nullable;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,19 +11,18 @@ import java.util.regex.Pattern;
 /**
  * Created by Стас on 26.05.2016.
  */
-public class FileWatcher implements Callable<Integer>, AutoCloseable {
+public class WordCounter implements Callable<Integer>, AutoCloseable {
 
-    //// TODO: 06.06.2016 Сделать рекурсивный обход всех директорий
     public static final String DIRECTORY_SEPARATOR = "\\";
     public static final String SPACE = " ";
 
-    private static String REGEXP_FOR_MATH = ".+.txt";
-    private static Pattern pattern = Pattern.compile(REGEXP_FOR_MATH);
+    private static String REGEXP_FOR_PATTERN = ".+.txt";
+    private static Pattern pattern = Pattern.compile(REGEXP_FOR_PATTERN);
     private static ExecutorService exec = Executors.newWorkStealingPool();
 
     private File directory;
 
-    public FileWatcher(File directory) {
+    public WordCounter(File directory) {
         this.directory = directory;
     }
 
@@ -29,12 +30,20 @@ public class FileWatcher implements Callable<Integer>, AutoCloseable {
         List<Callable<Integer>> callables = new ArrayList<>();
         String[] files = directory.list((dir, name) -> pattern.matcher(name).matches());
         for (String file : files) {
-            callables.add(new FileWatcher(new File(directory.getPath() + DIRECTORY_SEPARATOR + file)));
+            callables.add(new WordCounter(new File(directory.getPath() + DIRECTORY_SEPARATOR + file)));
         }
         return callables;
     }
 
+    @Nullable
     public int startThreads() {
+        /**
+         * Можна обернуть весь блок try в
+         * if(exec.invokeAll != null)
+         * и тогда анотацию Nullable,
+         * можно будет убрать..
+         * Твое мнение..?
+         */
         int answer = 0;
         try {
             List<Future<Integer>> futures = exec.invokeAll(getFiles());
@@ -43,6 +52,16 @@ public class FileWatcher implements Callable<Integer>, AutoCloseable {
             }
         } catch (InterruptedException | TimeoutException | ExecutionException e) {
             e.printStackTrace();
+        }
+        return answer;
+    }
+
+    public int startThreadsWithDirectory() {
+        int answer = new WordCounter(directory).startThreads();
+        for (File file : directory.listFiles()) {
+            if (file.isDirectory()) {
+                answer += new WordCounter(file).startThreads();
+            }
         }
         return answer;
     }
